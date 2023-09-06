9461fe322e33160d0013fe7f62c55cce6e642381
adarshasachan <55997162+adarshasachan@users.noreply.github.com>
2023-09-06 11:10:34 +0100

    Add files via upload

---
+++ scripts/config_xml.js
@@ -0,0 +1,46 @@
+var TSC = TSC || {};
+
+TSC.embedded_config_xml = '<x:xmpmeta tsc:version="2.0.1" xmlns:x="adobe:ns:meta/" xmlns:tsc="http://www.techsmith.com/xmp/tsc/">\
+   <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:xmp="http://ns.adobe.com/xap/1.0/" xmlns:xmpDM="http://ns.adobe.com/xmp/1.0/DynamicMedia/" xmlns:xmpG="http://ns.adobe.com/xap/1.0/g/" xmlns:xmpMM="http://ns.adobe.com/xap/1.0/mm/" xmlns:tscDM="http://www.techsmith.com/xmp/tscDM/" xmlns:tscIQ="http://www.techsmith.com/xmp/tscIQ/" xmlns:tscHS="http://www.techsmith.com/xmp/tscHS/" xmlns:stDim="http://ns.adobe.com/xap/1.0/sType/Dimensions#" xmlns:stFnt="http://ns.adobe.com/xap/1.0/sType/Font#" xmlns:exif="http://ns.adobe.com/exif/1.0" xmlns:dc="http://purl.org/dc/elements/1.1/">\
+      <rdf:Description dc:creator="Gavin Boyd" dc:date="2019-07-08 12:00:30 PM" dc:source="Camtasia,9.0.0,enu" dc:title="Lab02 - Installing Kofax TotalAgility" dc:description="Kofax Education Services" tscDM:keywords="Kofax Education Services" tscDM:originId="5DA7CB2A-321D-4D79-9971-BC3DD6D4B84C" tscDM:project="Lab02 - Installing Kofax TotalAgility">\
+         <xmpDM:duration xmpDM:scale="1/1000" xmpDM:value="2765900"/>\
+         <xmpDM:videoFrameSize stDim:unit="pixel" stDim:h="720" stDim:w="1280"/>\
+         <tsc:langName>\
+            <rdf:Bag>\
+               <rdf:li xml:lang="en-US">English</rdf:li></rdf:Bag>\
+         </tsc:langName>\
+         <xmpDM:Tracks>\
+            <rdf:Bag>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="TableOfContents" xmpDM:frameRate="f1000" xmpDM:trackName="Table of Contents">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:name="Introduction" xmpDM:startTime="0"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Installation Accounts" xmpDM:startTime="51633"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Create and Configure Service Account" xmpDM:startTime="94767"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Configure SQL Server Permissions" xmpDM:startTime="253133"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Installation Media" xmpDM:startTime="372967"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Pre-requisite Checker Utility" xmpDM:startTime="402100"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Run Setup.exe" xmpDM:startTime="582833"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Installation Wizard Screens" xmpDM:startTime="618167"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Licensing" xmpDM:startTime="910567"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Install Transformation Server Components" xmpDM:startTime="986900"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Post Install Checks" xmpDM:startTime="1158333"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Backup Databases" xmpDM:startTime="1315133"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Kofax Program Groups" xmpDM:startTime="1393767"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Open the Designer" xmpDM:startTime="1415867"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Open the Workspace" xmpDM:startTime="1475133"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Open Transformation Designer" xmpDM:startTime="1534100"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Configuration Utility" xmpDM:startTime="1804933"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Configure Server Settings" xmpDM:startTime="1910433"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Install Hotfix/Service Pack" xmpDM:startTime="2587367"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Repair/Uninstall" xmpDM:startTime="2699400"/></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                  </rdf:Description>\
+               </rdf:li>\
+            </rdf:Bag>\
+         </xmpDM:Tracks>\
+         <tscDM:controller>\
+            <rdf:Description xmpDM:name="tscplayer">\
+               <tscDM:parameters>\
+                  <rdf:Bag>\
+                     <rdf:li xmpDM:name="autohide" xmpDM:value="true"/><rdf:li xmpDM:name="autoplay" xmpDM:value="true"/><rdf:li xmpDM:name="loop" xmpDM:value="false"/><rdf:li xmpDM:name="searchable" xmpDM:value="false"/><rdf:li xmpDM:name="captionsenabled" xmpDM:value="false"/><rdf:li xmpDM:name="sidebarenabled" xmpDM:value="true"/><rdf:li xmpDM:name="unicodeenabled" xmpDM:value="false"/><rdf:li xmpDM:name="backgroundcolor" xmpDM:value="FFFFFF"/><rdf:li xmpDM:name="sidebarlocation" xmpDM:value="left"/><rdf:li xmpDM:name="endaction" xmpDM:value="stop"/><rdf:li xmpDM:name="endactionparam" xmpDM:value="true"/><rdf:li xmpDM:name="locale" xmpDM:value="en-US"/></rdf:Bag>\
+               </tscDM:parameters>\
+               <tscDM:controllerText>\
+                  <rdf:Bag>\
+                  </rdf:Bag>\
+               </tscDM:controllerText>\
+            </rdf:Description>\
+         </tscDM:controller>\
+         <tscDM:contentList>\
+            <rdf:Description>\
+               <tscDM:files>\
+                  <rdf:Seq>\
+                     <rdf:li xmpDM:name="0" xmpDM:value="Lab02 - Installing Kofax TotalAgility.mp4"/></rdf:Seq>\
+               </tscDM:files>\
+            </rdf:Description>\
+         </tscDM:contentList>\
+      </rdf:Description>\
+   </rdf:RDF>\
+</x:xmpmeta>';
